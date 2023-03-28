216fcdbf1b10505fbe74b802c73313136eccabd5
Mike <64697794+M-Chapman@users.noreply.github.com>
2023-03-28 16:47:07 +0100

    added loop through input name and return reversed name

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace reverseName;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter your name: ");
+        var name = Console.ReadLine();
+        if (!string.IsNullOrEmpty(name))
+        {
+            var reverseName = new char[name.Length];
+            for (var i = 0; i < name.Length; i++)
+            {
+                reverseName[i] = name[name.Length-1-i];
+            };
+            var reversedName = new string(reverseName);
+            System.Console.WriteLine(reversedName);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # reverseName
 A simple C# program that asks for a name and responds with the name reversed
+
+## Running
+```
+dotnet build
+dotnet run
+```
+## Process
+* Asks the user to input their name
+* Runs a for loop for the length of the input name
+* adds each character from the end of the name backwards to an array of characters
+* stores the reversed name char array as a string and outputs the value
+
+## Output
+```
+Enter your name: 
+Mike
+ekiM
+```
